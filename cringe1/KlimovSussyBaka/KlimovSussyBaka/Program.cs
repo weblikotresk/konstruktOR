a475ff4099c99e9d74ac8624c48ecf1ec6c46135
weblikotresk <vladislav021203@gmail.com>
2023-05-09 00:16:03 +0300

    cleaning up

--- cringe1/KlimovSussyBaka/KlimovSussyBaka/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int Oxxxymiron = Convert.ToInt16(Console.ReadLine());
-if (Oxxxymiron == 5050)
-{
-    Console.WriteLine("Перебив охрану");
-}
-else
-{
-    Console.WriteLine("Х*ле ты всё умничаешь сука, ты чо, пидор?");
-}
